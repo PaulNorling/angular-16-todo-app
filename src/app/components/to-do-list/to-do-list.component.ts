a8bb65e20a1c28309da7f2bf24d5b0352a95813e
Paul Norling <paulnorling87@gmail.com>
2024-04-19 14:25:06 -0500

    successful get

--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
+import { Task } from 'src/app/models/task';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
   styleUrls: ['./to-do-list.component.css']
 })
-export class ToDoListComponent {
+export class ToDoListComponent implements OnInit {
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('https://api.revel-health.com/channel-service/voice/callflow/dfad1f5a-4c32-4dd8-af6f-613ec78e3e13').subscribe(data => {
+      console.log(data); // Process the received data
+    });
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../models/task';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:3000';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Task[]> {
+    return this.http.get<Task[]>(baseUrl);
+  }
 }
